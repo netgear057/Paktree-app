7fafa34b7685bdf32c80ff7ed4fab25444a939c7
Netgear057 <netgear057@gmail.com>
2025-08-16 17:53:28 +0500

    user

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -133,7 +133,7 @@ const handleLogout = async () => {
 
 
             {/* Profile dropdown */}
-            {!isAuthenticated ? (
+            {!user ? (
   <div className="ml-3">
     <Link to="/login">
       <button className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
